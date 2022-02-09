ff8603d98d17c0a849634d8a5222ea1b407aec2f
慶悟 利部 <190800@jc-21.jp>
2022-02-09 09:40:50 +0900

    表示できた

--- src/GetTicketListServlet.java
+++ src/GetTicketListServlet.java
@@ -1,6 +1,11 @@
-
-
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -28,8 +33,51 @@ public class GetTicketListServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/getTicketList.jsp");
-		rd.forward(request, response);
-	}
 
+
+
+final String driverName = "com.mysql.jdbc.Driver";
+final String url = "jdbc:mysql://192.168.54.190:3306/jsonkadai05";
+final String id = "jsonkadai05";
+final String pass = "JsonKadai05";
+
+
+
+try {
+Class.forName(driverName);
+Connection connection = DriverManager.getConnection(url, id, pass);
+PreparedStatement st = connection.prepareStatement("select * from ticket_list ");
+ResultSet result = st.executeQuery();
+List<String[]> list = new ArrayList<>();
+
+while( result.next() == true) {
+String[] s = new String[4];
+
+
+
+s[0]=result.getString("TENPO_ID");
+s[1]=result.getString("TICKET_ID");
+s[2]=result.getString("TICKET_NAME");
+s[3]=result.getString("POINT");
+
+
+
+list.add(s);
 }
+
+
+
+request.setAttribute("json",list);
+request.getRequestDispatcher("/WEB-INF/jsp/getPoint.jsp").forward(request,response);
+
+} catch (ClassNotFoundException e ) {
+e.printStackTrace();
+} catch (SQLException e ) {
+e.printStackTrace();
+}
+}
+
+// RequestDispatcher rd =
+// request.getRequestDispatcher("/WEB-INF/jsp/getPoint.jsp");
+// rd.forward(request, response);
+	}
