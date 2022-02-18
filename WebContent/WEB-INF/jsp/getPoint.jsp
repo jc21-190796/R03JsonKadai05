a87733eb4e15d9f91b19362a980cb80b4b54ca38
慶悟 利部 <190800@jc-21.jp>
2022-02-18 17:06:34 +0900

    一部変更しました。

--- WebContent/WEB-INF/jsp/getPoint.jsp
+++ WebContent/WEB-INF/jsp/getPoint.jsp
@@ -1,7 +1,25 @@
-
+<%@page import="java.util.ArrayList"%>
+<%@page import="java.util.Optional"%>
+<%@page import="java.util.List"%>
 <%@ page language="java" contentType="application/json; charset=UTF-8"
-pageEncoding="UTF-8"%><% int point = (Integer)request.getAttribute("point");%>
-{"POINT":<%=point%>}
+    pageEncoding="UTF-8"%>
+
+<%
+Optional<List<String[]>>optList =
+Optional.ofNullable((List<String[]>)request.getAttribute("json"));
+List<String[]> list=new ArrayList<>();
+if(optList.isPresent()){
+list = optList.get();
+}
+%>
 
 
+<% for (String[] s : list){
+String a = s[0] ;
+String b = s[1];
+String c = s[2];
+%>
 
+{"ポイント":<%= c %> }
+
+<% } %>
--- src/GetTicketListServlet.java
+++ src/GetTicketListServlet.java
@@ -16,10 +16,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 
-import bean.Ticket;
-
-
-
 /**
  * Servlet implementation class GetPointServlet
  */
@@ -39,51 +35,49 @@ public class GetTicketListServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setCharacterEncoding("UTF-8");
-		response.setCharacterEncoding("UTF-8");
+		
 		final String driverName = "com.mysql.jdbc.Driver";
-		final String url = "jdbc:mysql://192.168.54.190:3306/jsonkadai05";
+    	final String url = "jdbc:mysql://192.168.54.190:3306/jsonkadai05";
 		final String id = "jsonkadai05";
 		final String pass = "JsonKadai05";
+		
 		try {
-		InitialContext ic = new InitialContext();
-		Class.forName(driverName);
-		Connection con = DriverManager.getConnection(url,id,pass);//コネクションを取得
-		String user_Id = request.getParameter("USER_ID");
-		String tenpo_Id = request.getParameter("TENPO_ID");
-		String sql1 = "SELECT * FROM ticket_list WHERE TENPO_ID = ? AND USER_ID = ?";
-		PreparedStatement st = con.prepareStatement(sql1);
-		ResultSet rsPoint = st.executeQuery();
-		String[] s = new String[4];
-		s[0]=rsPoint.getString("TENPO_ID");
-		s[1]=rsPoint.getString("TICKET_ID");
-		s[2]=rsPoint.getString("TICKET_NAME");
-		s[3]=rsPoint.getString("POINT");
-		int userPoint= 0;
-		while(rsPoint.next()) {
-		userPoint = rsPoint.getInt("Point");
+			Class.forName(driverName);
+			Connection connection=DriverManager.getConnection(url,id,pass);
+			PreparedStatement st = connection.prepareStatement(
+							"select TENPO_ID,TICKET_ID,TICKET_NAME,POINT from ticket_list where TENPO_ID=? AND point <= (select POINT from point_list where TENPO_ID=? AND USER_ID=?)"
+					//"select * from point"
+						);
+			
+			String ten=request.getParameter("TENPO_ID");
+			String use=request.getParameter("USER_ID");
+			
+			st.setString(1, ten);
+			st.setString(2, ten);
+			st.setString(3, use);
+			
+			ResultSet result = st.executeQuery();
+			
+			List<String[]> list=new ArrayList<>();
+			while( result.next() == true) {
+				String[] s=new String[4];
+				s[0]=result.getString("TENPO_ID");
+				s[1]=result.getString("TICKET_ID");
+				s[2]=result.getString("TICKET_NAME");
+				s[3]=result.getString("POINT");
+				list.add(s);	
+			}
+			request.setAttribute("list", list);
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/getTicketList.jsp");
+			rd.forward(request, response);
+			
+		} catch (ClassNotFoundException e ) {
+			// TODO 閾ｪ蜍慕函謌舌＆繧後◆ catch 繝悶Ο繝�繧ｯ
+			e.printStackTrace();
+		} catch (SQLException e ) {
+			// TODO 閾ｪ蜍慕函謌舌＆繧後◆ catch 繝悶Ο繝�繧ｯ
+			e.printStackTrace();
 		}
-		/****************************/
-		String sql2 = "SELECT TENPO_ID,TICKET_ID,TICKET_NAME,Point FROM ticket WHERE TENPO_ID = ? AND point <= ?";
-		st = con.prepareStatement(sql2);
-		st.setString(1, tenpo_Id);
-		st.setInt(2, userPoint);
-		ResultSet rsTicket = st.executeQuery();
-		List<Ticket> list = new ArrayList<Ticket>();
-		while(rsTicket.next()) {
-		int TeId = rsTicket.getInt("TENPO_ID");
-		int TiId = rsTicket.getInt("TICKET_ID");
-		String optName= rsTicket.getString("TICKET_NAME");
-		int point = rsTicket.getInt("Point");
-		list.add(new Ticket(TeId,TiId,optName,point));
-		}
-		request.setAttribute("list", list);
-		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/getTicketList.jsp");
-		rd.forward(request, response);
-		st.close();
-		con.close();
-		}catch(Exception e) {
-		e.printStackTrace();
-		}
-		}
+	}
+
 }
