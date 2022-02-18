4363bdc5c6983edfb6ce19ea8bd01817bef64ea7
慶悟 利部 <190800@jc-21.jp>
2022-02-18 16:15:27 +0900

    すべて変更

--- src/GetPointServlet.java
+++ src/GetPointServlet.java
@@ -47,48 +47,57 @@ super();
 */
 protected void doGet(HttpServletRequest request, HttpServletResponse response)
 throws ServletException, IOException {
+	request.setCharacterEncoding("UTF-8");
+	response.setCharacterEncoding("UTF-8");
+	
+	String tenpo_Id = request.getParameter("TENPO_ID");
+	String user_Id = request.getParameter("USER_ID");
+	
+	final String driverName = "com.mysql.jdbc.Driver";
+	final String url = "jdbc:mysql://192.168.54.190:3306/jsonkadai05";
+	final String id = "jsonkadai05";
+	final String pass = "JsonKadai05";
+//	String user_Id = "190000@jc-21.jp";
+//	String tenpo_Id = "0000000002";
+	try {
+		InitialContext 	ic 	= new InitialContext();	//データソースを取得する前処理
+		Class.forName(driverName);
+		Connection con = DriverManager.getConnection(url, id, pass);
 
-
-
-final String driverName = "com.mysql.jdbc.Driver";
-final String url = "jdbc:mysql://192.168.54.190:3306/jsonkadai05";
-final String id = "jsonkadai05";
-final String pass = "JsonKadai05";
-
-
-
-try {
-Class.forName(driverName);
-Connection connection = DriverManager.getConnection(url, id, pass);
-PreparedStatement st = connection.prepareStatement("select * from point_list ");
-ResultSet result = st.executeQuery();
-List<String[]> list = new ArrayList<>();
-
-while( result.next() == true) {
-String[] s = new String[3];
-
-
-
-s[0]=result.getString("TENPO_ID");
-s[1]=result.getString("USER_ID");
-s[2]=result.getString("POINT");
-
-
-
-list.add(s);
+		String sql = "SELECT * FROM point_list WHERE TENPO_ID = ? AND USER_ID = ?";
+		
+		PreparedStatement st = con.prepareStatement(sql);
+		ResultSet result = st.executeQuery();
+		String[] s = new String[3];
+		s[0]=result.getString("TENPO_ID");
+		s[1]=result.getString("USER_ID");
+		s[2]=result.getString("POINT");
+		
+		
+		int point= 0;
+		int line = 0;
+		while(result.next()) {
+			line++;
+			point = result.getInt("Point");
+		}
+		
+		if(line==0) {	//該当するデータがないときに登録
+			sql = "INSERT INTO Point(TENPO_ID,USER_ID,Point) VALUES(?,?,500)";
+			st = con.prepareStatement(sql);
+			st.setString(1,tenpo_Id);
+			st.setString(2,user_Id);
+			point = 500;
+			st.executeUpdate();
+		}
+		
+		request.setAttribute("point", point);
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/getPoint.jsp");
+		rd.forward(request, response);
+		
+		st.close();
+		con.close();
+	}catch(Exception e) {
+		e.printStackTrace();
+	}
 }
-
-
-
-request.setAttribute("json",list);
-request.getRequestDispatcher("/WEB-INF/jsp/getPoint.jsp").forward(request,response);
-
-} catch (ClassNotFoundException e ) {
-e.printStackTrace();
-} catch (SQLException e ) {
-e.printStackTrace();
 }
-}
-}
-// RequestDispatcher rd =
-// request.getRequestDispatcher("/WEB-INF/jsp/getPoint.jsp");
--- src/GetTicketListServlet.java
+++ src/GetTicketListServlet.java
@@ -7,12 +7,18 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.naming.InitialContext;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+import bean.Ticket;
+
+
 
 /**
  * Servlet implementation class GetPointServlet
@@ -33,51 +39,51 @@ public class GetTicketListServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-
-
-final String driverName = "com.mysql.jdbc.Driver";
-final String url = "jdbc:mysql://192.168.54.190:3306/jsonkadai05";
-final String id = "jsonkadai05";
-final String pass = "JsonKadai05";
-
-
-
-try {
-Class.forName(driverName);
-Connection connection = DriverManager.getConnection(url, id, pass);
-PreparedStatement st = connection.prepareStatement("select * from ticket_list ");
-ResultSet result = st.executeQuery();
-List<String[]> list = new ArrayList<>();
-
-while( result.next() == true) {
-String[] s = new String[4];
-
-
-
-s[0]=result.getString("TENPO_ID");
-s[1]=result.getString("TICKET_ID");
-s[2]=result.getString("TICKET_NAME");
-s[3]=result.getString("POINT");
-
-
-
-list.add(s);
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		final String driverName = "com.mysql.jdbc.Driver";
+		final String url = "jdbc:mysql://192.168.54.190:3306/jsonkadai05";
+		final String id = "jsonkadai05";
+		final String pass = "JsonKadai05";
+		try {
+		InitialContext ic = new InitialContext();
+		Class.forName(driverName);
+		Connection con = DriverManager.getConnection(url,id,pass);//コネクションを取得
+		String user_Id = request.getParameter("USER_ID");
+		String tenpo_Id = request.getParameter("TENPO_ID");
+		String sql1 = "SELECT * FROM ticket_list WHERE TENPO_ID = ? AND USER_ID = ?";
+		PreparedStatement st = con.prepareStatement(sql1);
+		ResultSet rsPoint = st.executeQuery();
+		String[] s = new String[4];
+		s[0]=rsPoint.getString("TENPO_ID");
+		s[1]=rsPoint.getString("TICKET_ID");
+		s[2]=rsPoint.getString("TICKET_NAME");
+		s[3]=rsPoint.getString("POINT");
+		int userPoint= 0;
+		while(rsPoint.next()) {
+		userPoint = rsPoint.getInt("Point");
+		}
+		/****************************/
+		String sql2 = "SELECT TENPO_ID,TICKET_ID,TICKET_NAME,Point FROM ticket WHERE TENPO_ID = ? AND point <= ?";
+		st = con.prepareStatement(sql2);
+		st.setString(1, tenpo_Id);
+		st.setInt(2, userPoint);
+		ResultSet rsTicket = st.executeQuery();
+		List<Ticket> list = new ArrayList<Ticket>();
+		while(rsTicket.next()) {
+		int TeId = rsTicket.getInt("TENPO_ID");
+		int TiId = rsTicket.getInt("TICKET_ID");
+		String optName= rsTicket.getString("TICKET_NAME");
+		int point = rsTicket.getInt("Point");
+		list.add(new Ticket(TeId,TiId,optName,point));
+		}
+		request.setAttribute("list", list);
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/getTicketList.jsp");
+		rd.forward(request, response);
+		st.close();
+		con.close();
+		}catch(Exception e) {
+		e.printStackTrace();
+		}
+		}
 }
-
-
-
-request.setAttribute("json",list);
-request.getRequestDispatcher("/WEB-INF/jsp/getPoint.jsp").forward(request,response);
-
-} catch (ClassNotFoundException e ) {
-e.printStackTrace();
-} catch (SQLException e ) {
-e.printStackTrace();
-}
-}
-
-// RequestDispatcher rd =
-// request.getRequestDispatcher("/WEB-INF/jsp/getPoint.jsp");
-// rd.forward(request, response);
-	}
